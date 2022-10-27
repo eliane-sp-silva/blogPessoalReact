94732d01509bcba658d2fd5ccff2978d2ed9c5f1
= <3lian3.pereira@gmail.com>
2022-10-27 14:17:10 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Home from './paginas/home/Home';
+
+
+// colocar as paginas aqui!!
+function App() {
+  return (
+   <>
+   <Home />
+   </>
+  );
+}
+
+export default App;
